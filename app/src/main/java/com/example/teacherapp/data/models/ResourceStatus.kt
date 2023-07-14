28ebab4cd3dd0d51addc5dfd293d898447c89f8c
Adrian <adi78900@gmail.com>
2023-07-14 22:17:30 +0200

    Remove unused files.

--- app/src/main/java/com/example/teacherapp/data/models/ResourceStatus.kt
+++
@@ -1,7 +0,0 @@
-package com.example.teacherapp.data.models
-
-sealed class ResourceStatus {
-    object Loading : ResourceStatus()
-    object Success : ResourceStatus()
-    object Error: ResourceStatus()
-}
