328a65bf63b48a037af505d8db5206a9088a95f7
Adrian <adi78900@gmail.com>
2023-07-17 16:40:37 +0200

    Add school year route.

--- app/src/main/java/com/example/teacherapp/ui/nav/graphs/schoolyear/SchoolYearNavigation.kt
+++ app/src/main/java/com/example/teacherapp/ui/nav/graphs/schoolyear/SchoolYearNavigation.kt
@@ -1,14 +1,10 @@
 package com.example.teacherapp.ui.nav.graphs.schoolyear
 
-import androidx.compose.runtime.LaunchedEffect
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
-import com.example.teacherapp.data.models.input.FormStatus
-import com.example.teacherapp.ui.screens.schoolyear.SchoolYearFormScreen
-import com.example.teacherapp.ui.screens.schoolyear.data.SchoolYearFormViewModel
+import com.example.teacherapp.ui.nav.graphs.schoolyear.route.SchoolYearFormRoute
 
 private const val schoolYearFormScreen = "school-year-form"
 private const val schoolYearFormRoute = schoolYearFormScreen
@@ -19,30 +15,6 @@ fun NavController.navigateToSchoolYearFormRoute(navOptions: NavOptions? = null) 
 
 fun NavGraphBuilder.schoolYearGraph(navController: NavController) {
     composable(schoolYearFormRoute) {
-        val viewModel = hiltViewModel<SchoolYearFormViewModel>()
-        val form = viewModel.form
-        val status = form.status
-
-        val onNavBack: () -> Unit = { navController.popBackStack() }
-
-        LaunchedEffect(status, onNavBack) {
-            if (status == FormStatus.Success) {
-                onNavBack()
-            }
-        }
-
-        SchoolYearFormScreen(
-            termForms = form.termForms,
-            showNavigationIcon = true,
-            onNavBack = onNavBack,
-            schoolYearName = form.schoolYearName,
-            onSchoolYearNameChange = viewModel::onSchoolYearNameChange,
-            onTermNameChange = viewModel::onTermNameChange,
-            onStartDateChange = viewModel::onStartDateChange,
-            onEndDateChange = viewModel::onEndDateChange,
-            status = form.status,
-            isSubmitEnabled = form.isSubmitEnabled,
-            onAddSchoolYear = viewModel::onAddSchoolYear,
-        )
+        SchoolYearFormRoute(showNavigationIcon = true, onNavBack = navController::popBackStack)
     }
 }
---
+++ app/src/main/java/com/example/teacherapp/ui/nav/graphs/schoolyear/route/SchoolYearFormRoute.kt
@@ -0,0 +1,38 @@
+package com.example.teacherapp.ui.nav.graphs.schoolyear.route
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.teacherapp.data.models.input.FormStatus
+import com.example.teacherapp.ui.screens.schoolyear.SchoolYearFormScreen
+import com.example.teacherapp.ui.screens.schoolyear.data.SchoolYearFormViewModel
+
+@Composable
+internal fun SchoolYearFormRoute(
+    showNavigationIcon: Boolean,
+    onNavBack: () -> Unit,
+) {
+    val viewModel = hiltViewModel<SchoolYearFormViewModel>()
+    val form = viewModel.form
+    val status = form.status
+
+    LaunchedEffect(status, onNavBack) {
+        if (status == FormStatus.Success) {
+            onNavBack()
+        }
+    }
+
+    SchoolYearFormScreen(
+        termForms = form.termForms,
+        showNavigationIcon = showNavigationIcon,
+        onNavBack = onNavBack,
+        schoolYearName = form.schoolYearName,
+        onSchoolYearNameChange = viewModel::onSchoolYearNameChange,
+        onTermNameChange = viewModel::onTermNameChange,
+        onStartDateChange = viewModel::onStartDateChange,
+        onEndDateChange = viewModel::onEndDateChange,
+        status = form.status,
+        isSubmitEnabled = form.isSubmitEnabled,
+        onAddSchoolYear = viewModel::onAddSchoolYear,
+    )
+}
