cb1e1cff87b616429f617f54e29e0c0fb54d99ac
Adrian <adi78900@gmail.com>
2023-07-18 20:17:08 +0200

    Add grades to student grades screen.

--- app/src/main/java/com/example/teacherapp/ui/screens/student/StudentGradesScreen.kt
+++ app/src/main/java/com/example/teacherapp/ui/screens/student/StudentGradesScreen.kt
@@ -2,6 +2,8 @@ package com.example.teacherapp.ui.screens.student
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -10,13 +12,16 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import com.example.teacherapp.core.common.result.Result
+import com.example.teacherapp.core.model.data.StudentGrade
 import com.example.teacherapp.core.model.data.StudentGradesByLesson
+import com.example.teacherapp.ui.components.form.TeacherOutlinedButton
 import com.example.teacherapp.ui.components.result.ResultContent
 import com.example.teacherapp.ui.screens.paramproviders.StudentGradesByLessonPreviewParameterProvider
 import com.example.teacherapp.ui.theme.TeacherAppTheme
@@ -54,12 +59,38 @@ private fun MainScreen(
         ) { index, studentGrade ->
             Text(studentGrade.lessonName)
             Text("Średnia: ${studentGrade.average.toPlainString()}")
-//            Text(studentGrade.gradesByLessonId.joinToString())
+            Grades(grades = studentGrade.gradesByLessonId)
 
             if (index != studentGradesByLesson.lastIndex) {
                 Divider()
             }
         }
+    }
+}
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+private fun Grades(
+    grades: List<StudentGrade>,
+    modifier: Modifier = Modifier,
+) {
+    FlowRow(modifier = modifier) {
+        for (grade in grades) {
+            TextButton(onClick = { /*TODO*/ }) {
+                Grade(grade = grade.grade.toPlainString(), onClick = {})
+            }
+        }
+    }
+}
+
+@Composable
+private fun Grade(
+    grade: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    TeacherOutlinedButton(modifier = modifier, onClick = onClick) {
+        Text(grade)
     }
 }
 
