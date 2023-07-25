ffcf6ff5cd6010b422af5466600937257d067919
Adrian <adi78900@gmail.com>
2023-07-25 15:24:09 +0200

    Remove unused files.

--- feature/lesson/src/main/java/com/example/teacherapp/feature/lesson/grade/data/GradesUiState.kt
+++
@@ -1,9 +0,0 @@
-package com.example.teacherapp.feature.lesson.grade.data
-
-import com.example.teacherapp.core.model.data.BasicGradeForTemplate
-import com.example.teacherapp.core.model.data.GradeTemplateInfo
-
-internal data class GradesUiState(
-    val grades: List<BasicGradeForTemplate>,
-    val gradeTemplateInfo: GradeTemplateInfo,
-)
