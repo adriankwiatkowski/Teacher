2fba14f6a818f52ecc36006e948aa629f974959e
Adrian <adi78900@gmail.com>
2023-07-26 12:03:21 +0200

    Add tonal elevation to whole app.

--- app/src/main/java/com/example/teacherapp/MainActivity.kt
+++ app/src/main/java/com/example/teacherapp/MainActivity.kt
@@ -5,13 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.teacherapp.core.common.result.Result
@@ -39,7 +39,7 @@ class MainActivity : ComponentActivity() {
             ) {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    tonalElevation = 5.dp,
                 ) {
                     TeacherApp()
                 }
@@ -64,10 +64,12 @@ private fun shouldShowDarkTheme(settingsData: SettingsData): Boolean {
     }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
 private fun DefaultPreview() {
     TeacherAppTheme {
-        TeacherApp()
+        Surface {
+            TeacherApp()
+        }
     }
 }
