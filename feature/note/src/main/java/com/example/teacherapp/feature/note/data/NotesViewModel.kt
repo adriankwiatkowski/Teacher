37d18eb528a35e138ca4a308ef8e59fdaf7f326d
Adrian <adi78900@gmail.com>
2023-07-26 11:59:36 +0200

    Fix NotesViewModel.kt.

--- feature/note/src/main/java/com/example/teacherapp/feature/note/data/NotesViewModel.kt
+++ feature/note/src/main/java/com/example/teacherapp/feature/note/data/NotesViewModel.kt
@@ -5,11 +5,13 @@ import androidx.lifecycle.viewModelScope
 import com.example.teacherapp.core.common.result.Result
 import com.example.teacherapp.core.data.repository.note.NoteRepository
 import com.example.teacherapp.core.model.data.Note
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
+@HiltViewModel
 internal class NotesViewModel @Inject constructor(repository: NoteRepository) : ViewModel() {
 
     val notesResult: StateFlow<Result<List<Note>>> = repository
