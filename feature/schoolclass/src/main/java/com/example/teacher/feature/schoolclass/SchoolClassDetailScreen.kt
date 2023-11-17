4e2c6859373ba834d934509b40ae706d6bb730c8
Adrian <adi78900@gmail.com>
2023-11-17 13:42:42 +0100

    Change SchoolClassDetailScreen.

--- feature/schoolclass/src/main/java/com/example/teacher/feature/schoolclass/SchoolClassDetailScreen.kt
+++ feature/schoolclass/src/main/java/com/example/teacher/feature/schoolclass/SchoolClassDetailScreen.kt
@@ -1,12 +1,16 @@
 package com.example.teacher.feature.schoolclass
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -15,6 +19,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,6 +27,7 @@ import androidx.compose.ui.tooling.preview.PreviewParameter
 import com.example.teacher.core.common.utils.TimeUtils
 import com.example.teacher.core.model.data.SchoolYear
 import com.example.teacher.core.ui.paramprovider.SchoolYearPreviewParameterProvider
+import com.example.teacher.core.ui.provider.TeacherIcons
 import com.example.teacher.core.ui.theme.TeacherTheme
 import com.example.teacher.core.ui.theme.spacing
 import java.time.LocalDate
@@ -51,9 +57,13 @@ private fun MainScreen(
     Column(
         modifier = modifier.verticalScroll(rememberScrollState()),
     ) {
-        Text(text = schoolYear.name)
+        Text(
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            text = schoolYear.name,
+            style = MaterialTheme.typography.titleLarge,
+        )
 
-        Spacer(Modifier.padding(MaterialTheme.spacing.small))
+        Spacer(Modifier.height(MaterialTheme.spacing.small))
 
         val firstTerm = schoolYear.firstTerm
         TermItem(
@@ -62,7 +72,7 @@ private fun MainScreen(
             endDate = firstTerm.endDate,
         )
 
-        Spacer(Modifier.padding(MaterialTheme.spacing.small))
+        Spacer(Modifier.height(MaterialTheme.spacing.large))
 
         val secondTerm = schoolYear.secondTerm
         TermItem(
@@ -81,16 +91,39 @@ private fun TermItem(
     modifier: Modifier = Modifier
 ) {
     Column(modifier = modifier) {
-        Text(text = stringResource(R.string.school_class_term_name, name))
+        Text(
+            text = stringResource(R.string.school_class_term_name, name),
+            style = MaterialTheme.typography.titleMedium,
+        )
 
-        Row {
-            Text(stringResource(R.string.school_class_term_start))
-            Text(TimeUtils.format(startDate))
-        }
-        Row {
-            Text(stringResource(R.string.school_class_term_end))
-            Text(TimeUtils.format(endDate))
-        }
+        TermDate(date = startDate)
+
+        Spacer(Modifier.height(MaterialTheme.spacing.small))
+
+        TermDate(date = endDate)
+    }
+}
+
+@Composable
+private fun TermDate(
+    date: LocalDate,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            modifier = Modifier.weight(1f, fill = false),
+            text = stringResource(R.string.school_class_term_start, TimeUtils.format(date)),
+        )
+        Spacer(Modifier.width(MaterialTheme.spacing.small))
+        Icon(
+            imageVector = TeacherIcons.date().icon,
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.primary,
+        )
     }
 }
 
