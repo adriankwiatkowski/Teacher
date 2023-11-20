961f6b4ae3616622afd555c12d69e7d444289bbc
Adrian <adi78900@gmail.com>
2023-11-20 22:58:18 +0100

    Change default grade score minimal thresholds.

--- core/database/src/main/java/com/example/teacher/core/database/constant/GradeScoreConstant.kt
+++ core/database/src/main/java/com/example/teacher/core/database/constant/GradeScoreConstant.kt
@@ -2,22 +2,22 @@ package com.example.teacher.core.database.constant
 
 internal object GradeScoreConstant {
 
-    const val DefaultTwoMinusMinThreshold = 20L
-    const val DefaultTwoMinThreshold = 25L
-    const val DefaultTwoPlusMinThreshold = 30L
+    const val DefaultTwoMinusMinThreshold = 25L
+    const val DefaultTwoMinThreshold = 30L
+    const val DefaultTwoPlusMinThreshold = 40L
 
-    const val DefaultThreeMinusMinThreshold = 35L
-    const val DefaultThreeMinThreshold = 40L
-    const val DefaultThreePlusMinThreshold = 45L
+    const val DefaultThreeMinusMinThreshold = 45L
+    const val DefaultThreeMinThreshold = 50L
+    const val DefaultThreePlusMinThreshold = 60L
 
-    const val DefaultFourMinusMinThreshold = 50L
-    const val DefaultFourMinThreshold = 55L
-    const val DefaultFourPlusMinThreshold = 60L
+    const val DefaultFourMinusMinThreshold = 65L
+    const val DefaultFourMinThreshold = 70L
+    const val DefaultFourPlusMinThreshold = 75L
 
-    const val DefaultFiveMinusMinThreshold = 65L
-    const val DefaultFiveMinThreshold = 70L
-    const val DefaultFivePlusMinThreshold = 75L
+    const val DefaultFiveMinusMinThreshold = 80L
+    const val DefaultFiveMinThreshold = 85L
+    const val DefaultFivePlusMinThreshold = 90L
 
-    const val DefaultSixMinusMinThreshold = 80L
-    const val DefaultSixMinThreshold = 85L
+    const val DefaultSixMinusMinThreshold = 95L
+    const val DefaultSixMinThreshold = 100L
 }
